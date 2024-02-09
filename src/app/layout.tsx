b7423a15d890c5dc0d67123e60787d50aa104f14
facuperezm <facundoperezmontalvo@gmail.com>
2024-02-09 17:40:58 -0300

    feat: add background in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
           inter.className
         )}
       >
-        {children}
+        <div className="relative h-full w-full bg-background">
+          <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+          {children}
+        </div>
       </body>
     </html>
   );
