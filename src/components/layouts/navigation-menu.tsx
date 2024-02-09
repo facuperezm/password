600e1da478670eacbf3271070bd9e4913004c6c9
facuperezm <facundoperezmontalvo@gmail.com>
2024-02-09 15:56:21 -0300

    feat: first iteration of navigation menu

---
+++ src/components/layouts/navigation-menu.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function NavigationMenu() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background">
+      <nav className="container flex items-center justify-between h-16 mx-auto">
+        <a href="/" className="flex items-center">
+          <span className="ml-2 text-2xl font-bold text-balance">KeyPass</span>
+        </a>
+        <ul className="flex items-center gap-4">
+          <li>
+            <Button variant="outline" asChild>
+              <Link href="/" className="text-balance">
+                Sign up
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button variant="ghost" asChild>
+              <Link href="/" className="text-balance">
+                Login
+              </Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
