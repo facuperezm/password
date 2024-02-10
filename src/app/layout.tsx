67f2948311ec16ae9c0423a23a64ce676266e452
facuperezm <facundoperezmontalvo@gmail.com>
2024-02-10 16:37:34 -0300

    fix: change name for variable storing the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Ubuntu } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,11 +20,12 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          inter.className
+          ubuntu.className
         )}
       >
         <div className="relative h-full w-full bg-background">
           <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+
           {children}
         </div>
       </body>
